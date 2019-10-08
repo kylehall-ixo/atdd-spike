9368941512cfca63db5f6a8ddacbf490c49875b3
Kyle Hall <kyle.d.hall@accenture.com>
2019-10-08 10:38:22 -0500

    first passing test

---
+++ features/is_it_friday_yet.feature
@@ -0,0 +1,7 @@
+Feature: Is It Friday, Yet?
+    Everybody wants to know when it's Friday
+
+    Scenario: Sunday isn't Friday
+        Given today is Sunday
+        When I ask if today is Friday
+        Then I should be told "Nope"
--- features/step_definitions/stepDefs.js
+++ features/step_definitions/stepDefs.js
@@ -1,3 +1,18 @@
 const assert = require('assert')
 const { Given, When, Then } = require('cucumber')
 
+function isItFriday(today) {
+    return "Nope"
+}
+
+Given('today is Sunday', function () {
+    this.today = 'Sunday'
+});
+
+When('I ask if today is Friday', function () {
+    this.actual = isItFriday(this.today)
+});
+
+Then('I should be told "Nope"', function () {
+  assert.equal(this.actual, "Nope")
+});
